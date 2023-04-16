291790b4084998e43118177c1c747cb4e0457452
JohnatanSSantos <johnatansantos317@gmail.com>
2023-04-16 16:23:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/smartcities/ApresentaMenuActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.smartcities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApresentaMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apresenta_menu_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/smartcities/ModeloMenuActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.smartcities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeloMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modelo_menu_layout);
+    }
+}
