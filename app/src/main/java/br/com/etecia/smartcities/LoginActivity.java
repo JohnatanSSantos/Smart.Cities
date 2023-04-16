18ca03d98fb7c2e14aa47ae17ef60c88c3a7d630
JohnatanSSantos <johnatansantos317@gmail.com>
2023-04-16 19:36:09 -0300

    Smart Cities - Login atualizado

--- app/src/main/java/br/com/etecia/smartcities/LoginActivity.java
+++ app/src/main/java/br/com/etecia/smartcities/LoginActivity.java
@@ -2,13 +2,67 @@ package br.com.etecia.smartcities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+
+        edtSenha = findViewById(R.id.edtSenha);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String usuario, senha;
+
+
+                usuario = edtUsuario.getText().toString();
+                senha = edtSenha.getText().toString();
+
+
+                if (usuario.equals("admin") && senha.equals("admin")) {
+
+                    startActivity(new Intent(getApplicationContext(),
+                            Menu.class));
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos!!!",
+                            Toast.LENGTH_SHORT).show();
+                    edtUsuario.setText("");
+                    edtSenha.setText("");
+                    edtUsuario.requestFocus();
+                }
+
+            }
+        });
+
     }
 }
